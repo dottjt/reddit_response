8a22e04bbff31f6d87cec5f9567cec005327effb
dottjt <julius_dott@hotmail.com>
2020-08-10 15:19:24 +1000

    yeahhh

--- src/tampermonkey/scripts/noFapNewSubreddit.js
+++ src/tampermonkey/scripts/noFapNewSubreddit.js
@@ -2490,11 +2490,11 @@
         forumType: ForumType.rNofapForum,
     };
     var R_NOFAP_USERNAME = 'hanyuqn';
-    var R_NOFAP_TIMESTAMP = '2 hours ago';
+    var R_NOFAP_TIMESTAMP = '3 hours ago';
     var R_PORN_FREE_USERNAME = 'ChewsOnRocks';
-    var R_PORN_FREE_TIMESTAMP = '1 hours ago';
+    var R_PORN_FREE_TIMESTAMP = '2 hours ago';
     var R_PORN_ADDICTION_USERNAME = 'Datthinker';
-    var R_PORN_ADDICTION_TIMESTAMP = '2 hours ago';
+    var R_PORN_ADDICTION_TIMESTAMP = '3 hours ago';
     var R_NOFAP_CHRISTIANS_USERNAME = '';
     var R_NOFAP_CHRISTIANS_TIMESTAMP = '';
     var R_NOFAP_TEENS_USERNAME = '';
